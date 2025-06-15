5e089bd40c3c3ba4f4d9804aeb7bb91dc503b345
Fabio Silveira <fabio.eds@hotmail.com>
2025-06-14 21:01:29 -0700

    AddedStyle

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import axios from "axios";
-import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
@@ -35,15 +34,15 @@ const Item = styled(Paper)(({ theme }) => ({
 export default function Home() {
 
     const [lanche, setLanche] = useState<Meal[]>([]);
-    // const [bebidas, setBebidas] = useState<Meal[]>([]);
+    const [bebidas, setBebidas] = useState<Meal[]>([]);
 
 
     useEffect(() => {
         async function fetchApi() {
             const reqLanche = await axios.get("https://67b5223ba9acbdb38ed16600.mockapi.io/api/v1/foods?tipo=lanche")
-            // const reqBebidas = await axios.get("https://67b5223ba9acbdb38ed16600.mockapi.io/api/v1/foods?tipo=bebida")
+            const reqBebidas = await axios.get("https://67b5223ba9acbdb38ed16600.mockapi.io/api/v1/foods?tipo=bebida")
             setLanche(reqLanche.data)
-            // setBebidas(reqBebidas.data)
+            setBebidas(reqBebidas.data)
         }
         fetchApi();
     }, [])
@@ -56,17 +55,106 @@ export default function Home() {
             <Container className="margin-top" fixed>
 
                 {lanche.map((e) => (
-                    <Box sx={{ flexGrow: 1 }} key={e.id}>
+                    <Box className="box-home" sx={{ flexGrow: 1 }} key={e.id} >
                         <Grid container spacing={2}>
                             <Grid size={2}>
                                 <Stack spacing={2}>
-                                    <Item>{e.nome}</Item>
-                                    <Item>{e.preco}</Item>
-                                    <Item></Item>
+                                    <Item sx={{
+                                        backgroundColor: '#ffe0c7',
+                                        color: '#e65100',
+                                        fontWeight: 500,
+                                        fontSize: '1rem',
+                                        borderRadius: 2,
+                                        padding: '12px'
+                                    }}>
+                                        {e.nome}
+                                    </Item>
+                                    <Item sx={{
+                                        backgroundColor: '#ffe0c7',
+                                        color: '#e65100',
+                                        fontWeight: 500,
+                                        fontSize: '1rem',
+                                        borderRadius: 2,
+                                        padding: '12px'
+                                    }}>
+                                        {e.preco}
+                                    </Item>
+                                    <Item sx={{
+                                        backgroundColor: '#ffe0c7',
+                                        color: '#e65100',
+                                        fontWeight: 500,
+                                        fontSize: '1rem',
+                                        borderRadius: 2,
+                                        padding: '12px'
+                                    }}>
+                                        {e.descricao}
+                                    </Item>
                                 </Stack>
                             </Grid>
                             <Grid size={4}>
-                                <Item sx={{ height: '100%', boxSizing: 'border-box' }}>
+                                <Item
+                                    sx={{
+                                        height: '100%',
+                                        boxSizing: 'border-box',
+                                        border: '2px solid #e65100',
+                                        borderRadius: 2, // optional for rounded corners
+                                        padding: 1, // optional spacing inside
+                                    }}
+                                >
+                                    <img src={e.imagem} className="img-home" />
+                                </Item>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                ))}
+
+                {bebidas.map((e) => (
+                    <Box className="box-home" sx={{ flexGrow: 1 }} key={e.id} >
+                        <Grid container spacing={2}>
+                            <Grid size={2}>
+                                <Stack spacing={2}>
+                                    <Item sx={{
+                                        backgroundColor: '#ffe0c7',
+                                        color: '#e65100',
+                                        fontWeight: 500,
+                                        fontSize: '1rem',
+                                        borderRadius: 2,
+                                        padding: '12px'
+                                    }}>
+                                        {e.nome}
+                                    </Item>
+                                    <Item sx={{
+                                        backgroundColor: '#ffe0c7',
+                                        color: '#e65100',
+                                        fontWeight: 500,
+                                        fontSize: '1rem',
+                                        borderRadius: 2,
+                                        padding: '12px'
+                                    }}>
+                                        ${e.preco}
+                                    </Item>
+                                    <Item sx={{
+                                        backgroundColor: '#ffe0c7',
+                                        color: '#e65100',
+                                        fontWeight: 500,
+                                        fontSize: '1rem',
+                                        borderRadius: 2,
+                                        padding: '12px'
+                                    }}>
+                                        {e.descricao}
+                                    </Item>
+                                </Stack>
+                            </Grid>
+                            <Grid size={4}>
+                                <Item
+                                    sx={{
+                                        height: '100%',
+                                        boxSizing: 'border-box',
+                                        border: '2px solid #e65100',
+                                        borderRadius: 2, // optional for rounded corners
+                                        padding: 1, // optional spacing inside
+                                    }}
+                                >
                                     <img src={e.imagem} className="img-home" />
                                 </Item>
                             </Grid>
