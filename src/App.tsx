1dcb72e918cd62c213e3761347a960e4a33f5da0
Fabio Silveira <fabio.eds@hotmail.com>
2025-07-15 23:55:21 -0700

    Added2Routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Home from "./pages/Home";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Sandwiches from "./pages/Sandwiches";
+import Desserts from "./pages/Desserts";
 
 export default function App() {
   return (
@@ -10,6 +12,8 @@ export default function App() {
       <Route path="/" element={<Home />}/>
       <Route path="/sign-in" element={<SignIn />}/>
       <Route path="/sign-up" element={<SignUp />}/>
+      <Route path="/sandwiches" element={<Sandwiches />}/>
+      <Route path="/desserts" element={<Desserts />}/>
     </Routes>
   ) 
 }
---
+++ src/pages/Sandwiches.tsx
@@ -0,0 +1,7 @@
+export default function Sandwiches() {
+    return(
+        <div>
+
+        </div>
+    )
+}
