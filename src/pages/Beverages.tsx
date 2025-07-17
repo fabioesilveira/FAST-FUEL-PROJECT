25576cc02ef679f8717ed4886aa7e445f02be239
Fabio Silveira <fabio.eds@hotmail.com>
2025-07-17 14:12:02 -0700

    AddedBeveragesPage

--- src/pages/Beverages.tsx
+++ src/pages/Beverages.tsx
@@ -1,7 +1,115 @@
+import { useEffect, useState } from "react";
+import Footer from "../components/Footer";
+import axios from "axios";
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+
+type Meal = {
+    id: string,
+    nome: string,
+    tipo: string,
+    descricao: string,
+    imagem: string,
+    preco: string
+    quantidade: number
+}
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: (theme.vars ?? theme).palette.text.secondary,
+    ...theme.applyStyles('dark', {
+        backgroundColor: '#1A2027',
+    }),
+}));
+
 export default function Beverages() {
-    return(
-        <div>
-            
-        </div>
+
+    const [data, setData] = useState<Meal[]>([])
+
+    useEffect(() => {
+        async function fetchApi() {
+            const req = await axios.get("https://67b5223ba9acbdb38ed16600.mockapi.io/api/v1/foods?tipo=bebida")
+            setData(req.data)
+
+        }
+        fetchApi()
+    }, [])
+
+    const imageStyles: { [id: string]: React.CSSProperties } = {
+        "5": { width: "190px", height: "150px", marginTop: "70px" }, // Coke
+        "6": { width: "255px", height: "255px", marginTop: "30px" }, // Sprite
+        "7": { width: "170px", height: "170px", marginTop: "55px" }, // Dr, Pepper
+        "8": { width: "140px", height: "145px", marginTop: "60px" }, // Fanta Orange
+        "9": { width: "255px", height: "180px", marginTop: "50px" }, // Diet Coke
+        "10": { width: "180px", height: "185px", marginTop: "40px" }, // Lemonade
+    };
+
+    return (
+
+        <Container className="margin-top" fixed>
+            <div className="products-wrapper">
+                {data.map((e, index) => (
+                    <Box
+                        className={`box-home product-card ${index % 2 !== 0 ? 'reverse' : ''}`}
+                        key={e.id}
+                    >
+                        <Box className="card-left">
+                            <Stack spacing={2}>
+                                <Item sx={{ backgroundColor: '#ffe0c7', color: '#e65100', width: '250px', fontWeight: 500, fontSize: '1rem', borderRadius: 2, padding: '12px' }}>
+                                    {e.nome}
+                                </Item>
+                                <Item sx={{ backgroundColor: '#ffe0c7', color: '#e65100', width: '250px', fontWeight: 500, fontSize: '1rem', borderRadius: 2, padding: '12px' }}>
+                                    ${e.preco}
+                                </Item>
+                                <Item sx={{ backgroundColor: '#ffe0c7', color: '#e65100', width: '250px', fontWeight: 500, fontSize: '1rem', borderRadius: 2, padding: '12px' }}>
+                                    {e.descricao}
+                                </Item>
+                                <Button
+                                    sx={{
+                                        backgroundColor: '#e65100',
+                                        color: '#ffe0c7',
+                                        fontWeight: 'bold',
+                                        '&:hover': {
+                                            backgroundColor: '#bf360c',
+                                        },
+                                    }}
+                                // onClick={() => handleOrder(e)}
+                                >
+                                    ADD TO CART
+                                </Button>
+                            </Stack>
+                        </Box>
+
+                        <Box className="card-right">
+                            <Item
+                                sx={{
+                                    height: '300px',
+                                    width: '270px',
+                                    boxSizing: 'border-box',
+                                    border: '2px solid #e65100',
+                                    borderRadius: 2,
+                                    padding: 1,
+                                }}
+                            >
+                                <img
+                                    key={e.id}
+                                    src={e.imagem}
+                                    alt={e.nome}
+                                    style={imageStyles[e.id] || { width: "160px", height: "160px", marginTop: "60px" }}
+                                />
+                            </Item>
+                        </Box>
+                    </Box>
+                ))}
+            </div>
+            <Footer />
+        </Container>
     )
 }
