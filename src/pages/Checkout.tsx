c39537565d1787dd98065ac65fd70ce816532c35
Fabio Silveira <fabio.eds@hotmail.com>
2025-08-27 22:58:34 -0700

    AddedCode

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,6 +1,12 @@
+import Container from '@mui/material/Container';
+
 export default function Checkout() {
     return(
         <>
+        <Container>
+            
+        </Container>
+        
         </>
     )
 }
