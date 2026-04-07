64eaf258f53faf93effea205d315cb6a6bae8c64
Fabio Silveira <fabio.eds@hotmail.com>
2026-04-06 18:19:54 -0700

    BGMobile

--- src/components/PageBgMobile.tsx
+++ src/components/PageBgMobile.tsx
@@ -12,14 +12,14 @@ export default function PageBgMobile({ children }: PageBgMobileProps) {
         minHeight: "100dvh",
         overflowX: "hidden",
         background: `
-  repeating-linear-gradient(
-    90deg,
-    #ffffff 0px,
-    #ffffff 23px,
-    rgba(255, 248, 235, 0.38) 23px,
-    rgba(255, 248, 235, 0.38) 50px
-  )
-`,
+          repeating-linear-gradient(
+            90deg,
+            #ffffff 0px,
+            #ffffff 32px,
+            rgba(255, 248, 235, 0.36) 32px,
+            rgba(255, 248, 235, 0.36) 52px
+          )
+        `,
       }}
     >
       {children}
