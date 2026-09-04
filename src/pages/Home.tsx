5ae3a1752bab6839abd40ca97f23610e08146777
Fabio Silveira <fabio.eds@hotmail.com>
2026-09-04 12:27:08 -0700

    combo img % increase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -49,7 +49,7 @@ const mobileSlides = [
         id: "combo",
         src: ComboMobile,
         alt: "Combo Promo",
-        position: "center 62%",
+        position: "center 66%",
     },
     { id: "rest", src: RestImg, alt: "Rest" },
     { id: "girl", src: Chat6, alt: "Girl" },
