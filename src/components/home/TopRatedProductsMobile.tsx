5cc0dfb1a12e1ca936756799b3212d76f327f22a
Fabio Silveira <fabio.eds@hotmail.com>
2026-09-06 19:21:07 -0700

    mt to img 4

--- src/components/home/TopRatedProductsMobile.tsx
+++ src/components/home/TopRatedProductsMobile.tsx
@@ -64,7 +64,7 @@ export default function TopRatedProductsMobile({
     const imageStylesTopRated: Record<string, React.CSSProperties> = {
 
         "4": { width: "170px", height: "130px", marginTop: "27px" },
-        "10": { width: "160px", height: "162px", marginTop: "18px" },
+        "10": { width: "160px", height: "162px", marginTop: "20px" },
         "12": { width: "155px", height: "142px", marginTop: "25px" },
 
     };
