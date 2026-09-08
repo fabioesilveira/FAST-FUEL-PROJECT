8b027fea7ee9685cc0c8528b13fc5965189609ac
Fabio Silveira <fabio.eds@hotmail.com>
2026-09-07 19:46:43 -0700

    test

--- src/components/home/TopRatedProductsMobile.tsx
+++ src/components/home/TopRatedProductsMobile.tsx
@@ -75,7 +75,7 @@ export default function TopRatedProductsMobile({
     return (
         <Box
             sx={{
-                mt: 2.6,
+                mt: 2.5,
                 px: 2,
                 width: "100%",
             }}
