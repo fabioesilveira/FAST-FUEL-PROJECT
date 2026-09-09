14c71c435860308aefea3c170c4f2dc3ca62164c
Fabio Silveira <fabio.eds@hotmail.com>
2026-09-08 19:08:59 -0700

    fix pt + safe area mobile payment screens

--- src/components/checkout/CheckoutConfirmedScreen.tsx
+++ src/components/checkout/CheckoutConfirmedScreen.tsx
@@ -40,7 +40,7 @@ export default function CheckoutConfirmedScreen({
                     display: "flex",
                     justifyContent: "center",
                     px: 0,
-                    pt: "calc(40px + env(safe-area-inset-top))",
+                    pt: "calc(60px + env(safe-area-inset-top))",
                     pb: "calc(24.5px + env(safe-area-inset-bottom))",
                     bgcolor: "#fff",
                 }}
