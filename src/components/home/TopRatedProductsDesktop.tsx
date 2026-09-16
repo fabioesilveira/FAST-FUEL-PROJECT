f2db43ad34cae372d63b4e0648a5a4a2bac57dad
Fabio Silveira <fabio.eds@hotmail.com>
2026-09-16 15:09:03 -0700

    test

--- src/components/home/TopRatedProductsDesktop.tsx
+++ src/components/home/TopRatedProductsDesktop.tsx
@@ -85,12 +85,12 @@ export default function TopRatedProductsDesktop({
         "10": {
             width: "135px",
             height: "138px",
-            marginTop: "13px",
+            marginTop: "11px",
         },
         "12": {
             width: "132px",
             height: "122px",
-            marginTop: "22px",
+            marginTop: "20px",
         },
     };
 
