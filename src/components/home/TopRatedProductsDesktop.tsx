70b98305982af43d86ee4e5bfcce38737d5495d9
Fabio Silveira <fabio.eds@hotmail.com>
2026-09-19 11:27:18 -0700

    test

--- src/components/home/TopRatedProductsDesktop.tsx
+++ src/components/home/TopRatedProductsDesktop.tsx
@@ -43,7 +43,7 @@ const imageStylesTopRated: Record<string, React.CSSProperties> = {
     "10": {
         width: "135px",
         height: "138px",
-        transform: "translateY(1px)",
+        transform: "translateY(-0.5px)",
     },
     "12": {
         width: "132px",
